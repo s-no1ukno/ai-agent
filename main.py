b38535b8ef6767f83ec7ec67c6941026544afb5e
Jordan Snow <jordan@snowdev.co>
2025-11-04 20:56:02 -0700

    More setup and hookup

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+model = "gemini-2.0-flash-001"
+
+
 def main():
-    print("Hello from ai-agent!")
+    if len(sys.argv) < 2:
+        print("Error: Must include at least one argument.")
+        sys.exit(1)
+    user_prompt = sys.argv[1]
+
+    messages = [types.Content(
+        role="user", parts=[types.Part(text=user_prompt)])]
+
+    response = client.models.generate_content(model=model, contents=messages)
+    print(response.text)
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
