51c8045938a96f2a4d0eb7f9125ac0eb3871cff9
Jordan Snow <jordan@snowdev.co>
2025-11-06 21:56:01 -0700

    Adding --verbose flag

--- main.py
+++ main.py
@@ -15,7 +15,16 @@ def main():
     if len(sys.argv) < 2:
         print("Error: Must include at least one argument.")
         sys.exit(1)
-    user_prompt = sys.argv[1]
+
+    verbose = False
+    userinput = sys.argv[1:]
+
+    if "--verbose" in sys.argv:
+        verbose = True
+        userinput = list(filter(lambda entry: entry !=
+                         "--verbose", sys.argv[1:]))
+
+    user_prompt = userinput[0]
 
     messages = [types.Content(
         role="user", parts=[types.Part(text=user_prompt)])]
@@ -23,8 +32,12 @@ def main():
     response = client.models.generate_content(model=model, contents=messages)
     print(response.text)
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose is True:
+        print(f"User prompt: {user_prompt}")
+
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {
+              response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
